e328f6dc43251467d104d1d337c10a0951ec7cf5
fatihylmz1 <fatihylmz27@icloud.com>
2024-07-24 23:59:38 +0300

    Projects added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,9 @@
 import React from "react";
 import profilePhoto from "../Assests/cvphoto.jpg"
+import ecommercebackend from "../Assests/ecommercebackend.jpeg"
+import ecommerce from "../Assests/ecommerce.png"
+import gratitude from "../Assests/gratitude.jpeg"
+import library from "../Assests/library.jpeg"
 
 export const Home = () => {
 
@@ -12,6 +16,38 @@ export const Home = () => {
         link.click();
         document.body.removeChild(link);
     }
+
+    const projects = [
+        {
+            projectName: "E-Commerce Backend",
+            img: ecommercebackend,
+            date: 2024,
+            detail: "I developed the backend of an e-commerce project using Java, Spring Boot and PostgreSQL. In this project, I stored addresses, card information, user roles, products, and product categories in the database.",
+            tech: "Java, Spring Boot, PostgreSQL",
+        },
+        {
+            projectName: "E-Commerce",
+            img: ecommerce,
+            date: 2023,
+            detail: "This project is a web application built using React, a popular JavaScript library for building user interfaces. It leverages Redux for state management, providing a predictable state container for managing the application’s data flow. Additionally, Tailwind CSS is utilized for styling, offering a utility-first CSS framework that allows for rapid development and easy customization.",
+            tech: "JavaScript, React, Redux, Tailwind, HTML, CSS",
+        },
+        {
+            projectName: "Gratitude Journal",
+            img: gratitude,
+            date: 2023,
+            detail: "I developed a gratitude journal project where we can make API requests using redux-thunk and preserve added entries using localStorage. The user interface was built with React.",
+            tech: "JavaScript, React, Redux, HTML, CSS",
+        },
+        {
+            projectName: "Library Application with Java",
+            img: library,
+            date: 2024,
+            detail: "I developed a library system using Java, Spring Boot, and PostgreSQL. This project was designed using Object-Oriented Programming (OOP) principles.I developed a library system using Java, Spring Boot, and PostgreSQL.",
+            tech: "Java, Spring Boot, PostgreSQL",
+        },
+
+    ]
 
 
     return (
@@ -36,6 +72,25 @@ export const Home = () => {
                 </div>
                 <div>
                     <img src={profilePhoto} className="w-[50rem] h-[17rem] rounded-full object-cover object-top " />
+                </div>
+            </div>
+
+            <div className="flex flex-col gap-16 px-[10rem] py-[2rem]">
+                <div><p className="text-2xl font-bold">Featured Works</p></div>
+                <div className="flex flex-col gap-12">
+                    {projects.map((project, index) => (
+                        <div key={index} className="flex flex-row gap-8 items-center border p-6 rounded-lg shadow-lg">
+                            <div className="w-[30rem] h-[20rem]">
+                                <img src={project.img} alt={project.projectName} className="w-full h-full object-fill rounded-lg" />
+                            </div>
+                            <div className="flex flex-col gap-4 w-full">
+                                <p className="text-2xl font-bold">{project.projectName}</p>
+                                <p className="font-semibold text-center text-white bg-sky-800 rounded-full w-[4rem]">{project.date}</p>
+                                <p className="font-semibold">{project.detail}</p>
+                                <p className="font-semibold text-sky-800">{project.tech}</p>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
